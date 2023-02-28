eba1ba8650480126fd2d337d44ef1a8ed7ffebae
pawelstasiewicz <pst357357@gmail.com>
2023-02-28 21:05:01 +0100

    Update style, add day when someone can take car and try make accessories

--- app/js/script.js
+++ app/js/script.js
@@ -6,6 +6,9 @@ const selectedCar = document.querySelector('.selected_car');
 const priceArea = document.querySelector('.price p');
 const carPrice = document.querySelectorAll('.car_price');
 const returnBtn = document.querySelector('.return');
+const date = document.querySelector('.date');
+const wheelsBtn = document.querySelector('.wheelsBtn');
+const guaranteeBtn = document.querySelector('.guaranteeBtn');
 
 const main = () => {
 	prepareDOMEvents();
@@ -13,41 +16,60 @@ const main = () => {
 
 const prepareDOMEvents = () => {
 	returnBtn.addEventListener('click', closeForm);
+	wheelsBtn.addEventListener('click', addWheelsPrice);
 };
 
+//After click cars img form should appear with name of chosen car and price
 const clickCarsImg = (e) => {
 	form.classList.add('active');
 	setTimeout(() => {
 		mainArea.classList.replace('main', 'zero');
-	}, 100);
+	}, 1000);
 	if (e.target === carsImg[0]) {
 		selectedCar.textContent += ' ' + carName[0].textContent;
-		priceArea.textContent = carPrice[0].textContent + ' PLN';
+		priceArea.textContent = carPrice[0].textContent;
 	} else if (e.target === carsImg[1]) {
 		selectedCar.textContent += ' ' + carName[1].textContent;
-		priceArea.textContent = carPrice[1].textContent + ' PLN';
+		priceArea.textContent = carPrice[1].textContent;
 	} else if (e.target === carsImg[2]) {
 		selectedCar.textContent += ' ' + carName[2].textContent;
-		priceArea.textContent = carPrice[2].textContent + ' PLN';
+		priceArea.textContent = carPrice[2].textContent;
 	} else if (e.target === carsImg[3]) {
 		selectedCar.textContent += ' ' + carName[3].textContent;
-		priceArea.textContent = carPrice[3].textContent + ' PLN';
+		priceArea.textContent = carPrice[3].textContent;
 	} else if (e.target === carsImg[4]) {
 		selectedCar.textContent += ' ' + carName[4].textContent;
-		priceArea.textContent = carPrice[4].textContent + ' PLN';
+		priceArea.textContent = carPrice[4].textContent;
 	} else if (e.target === carsImg[5]) {
 		selectedCar.textContent += ' ' + carName[5].textContent;
-		priceArea.textContent = carPrice[5].textContent + ' PLN';
+		priceArea.textContent = carPrice[5].textContent;
 	}
 };
 
+//Date when car can be taken(2 weeks after)
+const now = new Date();
+const future_date = new Date();
+
+future_date.setDate(now.getDate() + 14);
+let html_date = future_date.toLocaleDateString();
+
+date.textContent = html_date;
+
+//Close form
 const closeForm = () => {
 	mainArea.classList.replace('zero', 'main');
-	//przeskakuje na ustawienia główne nie z media
 	form.classList.remove('active');
 	setTimeout(() => {
 		selectedCar.textContent = 'Wybrałeś ';
 	}, 1000);
+};
+
+//Try to add extra price for accessories - wheels
+const addWheelsPrice = () => {
+	wheelsBtn.classList.toggle('colorBtn');
+	const newPrice = priceArea.textContent;
+	let wheelsPrice = Number(newPrice) + 2000;
+	priceArea.textContent = wheelsPrice;
 };
 
 carsImg.forEach((item) => {
